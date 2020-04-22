8025bf2b3145c7358c53ab79c2860f4a452e60d4
Bernardo Escoffié Torre <escoffie@think-comunicacion.com>
2020-04-22 10:38:32 -0500

    Installed WooCommerce, basic SCSS setup

--- themes/think/functions.php
+++ themes/think/functions.php
@@ -8,3 +8,4 @@ require get_template_directory() . '/functions-parts/customizer.php';
 require get_template_directory() . '/functions-parts/widget-areas.php'; 
 require get_template_directory() . '/functions-parts/theme-support.php'; 
 require get_template_directory() . '/functions-parts/cf7-referer.php'; 
+require get_template_directory() . '/functions-parts/woocommerce.php'; 
